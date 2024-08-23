6f85ebda8eee418d5e47d5ab3ad80c965edd3dc5
Tomek Sosinski <tomek.sosinski@gmail.com>
2024-08-23 18:50:51 +0200

    New type ToastPosition

---
+++ src/params/lang.ts
@@ -0,0 +1,6 @@
+// ./src/params/lang.ts
+import { availableLanguageTags, type AvailableLanguageTag } from "$paraglide/runtime"
+
+export const match = (param: 'en' | 'de' | 'fr' | 'pl'): param is AvailableLanguageTag => {
+	return availableLanguageTags.includes(param)
+}
