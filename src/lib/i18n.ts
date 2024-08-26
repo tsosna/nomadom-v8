6c0fc20fc85f459ac1bc4fb03a084968d1b435f4
Tomek Sosinski <tomek.sosinski@gmail.com>
2024-08-26 15:51:44 +0200

    chore: Update i18n configuration and refactor Language component
    Refactor the Language.svelte component and update translations. Also, update the i18n configuration with the default language and text direction. This commit improves the UI localization by adding language-specific about pages and a Language component. It also updates the import path for availableLanguageTags in lang.ts.

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -2,11 +2,6 @@
 import { createI18n } from "@inlang/paraglide-sveltekit"
 import * as runtime from "$lib/paraglide/runtime.js"
 
-export const i18n = createI18n(runtime)
-// file initialized by the Paraglide-SvelteKit CLI - Feel free to edit it
-import { createI18n } from "@inlang/paraglide-sveltekit"
-import * as runtime from "$lib/paraglide/runtime.js"
-
 export const i18n = createI18n(runtime,{
 // pathnames - wyłączone z powodu nie kompatybilności z Vercelem
   pathnames: {
