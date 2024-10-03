9be73d47344db83a28c3184fbd392744208a34a1
Tomek Sosinski <tomek.sosinski@gmail.com>
2024-10-03 16:03:46 +0200

    refactor: Update footer component in app-shell.svelte

--- src/lib/components/footer/footer.svelte
+++ src/lib/components/footer/footer.svelte
@@ -1,27 +1,27 @@
 <script lang="ts">
-import { cn } from '@/utils'
-import { companyName, copyright,clickContinue, termsService, privacyPolicy } from '@/paraglide/messages'
-
-type Props = {
-  class?: string
-}
-
-let { class: CLASS }:Props = $props()
-
-</script>
-
-<footer class="text-xs">
-  <p class={cn("px-8 text-center text-sm text-muted-foreground", CLASS)}>
-  &copy <span class="uppercase">{companyName()}</span>
-  {new Date().getFullYear()}
-  {copyright()}
-    {clickContinue()}
-    <a href="/terms-of-service" class="underline underline-offset-4 hover:text-primary">
-      {termsService()}
-    </a>
-    and
-    <a href="/privacy" class="underline underline-offset-4 hover:text-primary">
-      {privacyPolicy()}</a
-    >
-  </p>
-</footer>
+  import { cn } from '@/utils'
+  import { companyName, copyright,clickContinue, termsService, privacyPolicy } from '$paraglide/messages'
+  
+  type Props = {
+    class?: string
+  }
+  
+  let { class: CLASS }:Props = $props()
+  
+  </script>
+  
+  <footer class="text-xs">
+    <p class={cn("px-8 text-center text-sm text-muted-foreground", CLASS)}>
+    &copy <span class="uppercase">{companyName()}</span>
+    {new Date().getFullYear()}
+    {copyright()}
+      {clickContinue()}
+      <a href="/terms-of-service" class="underline underline-offset-4 hover:text-primary">
+        {termsService()}
+      </a>
+      and
+      <a href="/privacy" class="underline underline-offset-4 hover:text-primary">
+        {privacyPolicy()}</a
+      >
+    </p>
+  </footer>
