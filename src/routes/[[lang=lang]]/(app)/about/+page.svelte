bd8e18b0ed55cf3af103f9b2f3a0af2a3d6a78d1
Tomek Sosinski <tomek.sosinski@gmail.com>
2024-10-04 14:04:09 +0200

    refactor: Comment out unused imports and component in about page

--- src/routes/[[lang=lang]]/(app)/about/+page.svelte
+++ src/routes/[[lang=lang]]/(app)/about/+page.svelte
@@ -1,24 +1,24 @@
 <script lang="ts">
-	// import en from './en.svelte'
-	// import de from './de.svelte'
-	// import pl from './pl.svelte'
-	// import fr from './fr.svelte'
+	import en from './en.svelte'
+	import de from './de.svelte'
+	import pl from './pl.svelte'
+	import fr from './fr.svelte'
 	import { about } from '$paraglide/messages.js'
-	// import { languageTag, type AvailableLanguageTag } from '$paraglide/runtime'
+	import { languageTag, type AvailableLanguageTag } from '$paraglide/runtime'
   import { goto } from '$app/navigation'
   import { i18n } from '$lib/i18n'
   import {home} from '@/paraglide/messages'
 	import { base } from '$app/paths'
 	import {Button} from '@/components/ui/button'
 
-	// const lang: Record<AvailableLanguageTag, any> = { en, de, pl, fr }
+	const lang: Record<AvailableLanguageTag, any> = { en, de, pl, fr }
 </script>
 
 <svelte:head>
 	<title>{about()}</title>
 </svelte:head>
 
-<!-- <svelte:component this={lang[languageTag()]} /> -->
+<svelte:component this={lang[languageTag()]} />
 
 <h1>
   About Page
