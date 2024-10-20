6fb7d3bc816a041b6e553bd9e37404c0c12a1c4d
Tomek Sosinski <tomek.sosinski@gmail.com>
2024-10-20 13:12:21 +0200

    Refactor: Comment out unused code in prisma.ts

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,15 +1,15 @@
 import { dev } from '$app/environment'
-const { PrismaClient } = dev ? await import('@prisma/client') : await import('@prisma/client/edge');
+// const { PrismaClient } = dev ? await import('@prisma/client') : await import('@prisma/client/edge');
+import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
+// 	// @ts-expect-error __prisma does exist globally declaration in src/app.d.ts
 const prisma = new PrismaClient().$extends(withAccelerate())
 
-console.log({dev});
-
-if (dev) {
-	// @ts-expect-error __prisma does exist globally declaration in src/app.d.ts
-	global.__prisma = prisma
-}
+// if (dev) {
+// 	// @ts-expect-error __prisma does exist globally declaration in src/app.d.ts
+// 	global.__prisma = prisma
+// }
 
 export { prisma }
 
