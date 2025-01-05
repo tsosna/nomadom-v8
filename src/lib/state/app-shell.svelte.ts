182b5a62cb44ef3b9a34b68f1895b603c0dbd317
Tomek Sosinski <tomek.sosinski@gmail.com>
2025-01-05 15:54:46 +0100

    Refactor: streamline openNavSide state management and toggle function in app-shell

--- src/lib/state/app-shell.svelte.ts
+++ src/lib/state/app-shell.svelte.ts
@@ -1,17 +1,17 @@
 import { getContext, setContext } from 'svelte'
 
 export class OpenNavSide {
-  openNavSide = $state(false)
+	openNavSide = $state(false)
 
-  // constructor() {
-  //   $effect(() => {
-  //     console.log('openNavSide', this.openNavSide);
-  //   })
-  // }
+	// constructor() {
+	//   $effect(() => {
+	//     console.log('openNavSide', this.openNavSide);
+	//   })
+	// }
 
-  toggleNavSide = (open: boolean) => {
-    this.openNavSide.set(open);
-  }
+	toggleNavSide = (open: boolean) => {
+		this.openNavSide = open
+	}
 }
 
 const OPEN_NAV_SIDE_KEY = Symbol('OPEN_NAV_SIDE')
@@ -21,9 +21,9 @@ const OPEN_NAV_SIDE_KEY = Symbol('OPEN_NAV_SIDE')
  * @returns {void}
  */
 export function setOpenNavSideState() {
-  return setContext(OPEN_NAV_SIDE_KEY, new OpenNavSide())
+	return setContext(OPEN_NAV_SIDE_KEY, new OpenNavSide())
 }
 
 export function getOpenNavSideState() {
-  return getContext<ReturnType<typeof setOpenNavSideState>>(OPEN_NAV_SIDE_KEY)
+	return getContext<ReturnType<typeof setOpenNavSideState>>(OPEN_NAV_SIDE_KEY)
 }
